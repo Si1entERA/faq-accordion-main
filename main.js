4a731d5ff964b4b6d5fd134b1535612b559019d7
Si1entERA <NeoLegote@outlook.com>
2024-07-15 22:43:13 +0200

    code added with layout changes

---
+++ main.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('.btn');
+const answers = document.querySelectorAll('.answer');
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const currentQuestion = e.currentTarget.closest('.faq-box');
+    const currentAnswer = currentQuestion.querySelector('.answer');
+
+    answers.forEach(answer => {
+      answer.style.display = 'none';
+      answer.previousElementSibling.querySelector('.btn').src = './assets/images/icon-plus.svg';
+    });
+
+    if (currentAnswer.style.display === 'block') {
+      currentAnswer.style.display = 'none';
+      e.currentTarget.src = './assets/images/icon-plus.svg';
+    } else {
+      currentAnswer.style.display = 'block';
+      e.currentTarget.src = './assets/images/icon-minus.svg';
+    }
+  });
+});
