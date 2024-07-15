2f3b318949d019b8250ca531ccdae865c9451d7c
Si1entERA <NeoLegote@outlook.com>
2024-07-15 23:05:18 +0200

    update

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ buttons.forEach((btn) => {
     const currentQuestion = e.currentTarget.closest('.faq-box');
     const currentAnswer = currentQuestion.querySelector('.answer');
 
+    //hide answer
     answers.forEach(answer => {
       answer.style.display = 'none';
       answer.previousElementSibling.querySelector('.btn').src = './assets/images/icon-plus.svg';
     });
 
+    //toggle between FAQ'S
     if (currentAnswer.style.display === 'block') {
       currentAnswer.style.display = 'none';
       e.currentTarget.src = './assets/images/icon-plus.svg';
